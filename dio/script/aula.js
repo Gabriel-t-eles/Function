8251b1c1d17d1965cc66584765292a996eab2e0e
Gabriel-t-eles <131554267+Gabriel-t-eles@users.noreply.github.com>
2023-06-21 20:46:09 -0300

    Update aula.js

--- dio/script/aula.js
+++ dio/script/aula.js
@@ -1,3 +1,4 @@
+/*
 function calcularImc(peso, altura){
     return peso/ Math.pow(altura, 2);
 }
@@ -24,4 +25,47 @@ const altura = 1.78
 const imc = calcularImc(peso, altura);
 console.log(classificarImc(imc),imc)
 })();
+*/
+
+/*
+function escreverNome(nome){
+return 'Meu nome é ' + nome ;
+}
+
+
+function maiorDeIdade(idade){
+    if(idade >= 18){
+        console.log(escreverNome('Teles') + ' Você é maior de idade');
+    }else{
+        console.log('Você é menor de idade');
+    }
+}
+
+maiorDeIdade(18)
+maiorDeIdade(15);
+*/
+
+function aplicarDesconto(valor, desconto){
+    return (valor - (valor * (desconto/100)));
+}
+function aplicarJuros(valor, juros){
+    return (valor + (valor * (juros/100)));
+}
+
+
+
+const x = 100;
+const formaDePagamento = '4';
+
+if(formaDePagamento === '1'){
+    console.log(aplicarDesconto(x,10));
+}else if(formaDePagamento === '2'){
+    console.log(aplicarDesconto(x,15));
+}else if(formaDePagamento === '3'){
+    console.log(x);
+}else if(formaDePagamento === '4'){
+    console.log(aplicarJuros(x,10));
+}else{
+    console.log('Informe a Forma de Pagamento')
+}
 
